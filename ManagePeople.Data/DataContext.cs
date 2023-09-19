69da5b5aecdbd6798fbf7f97d7dfee9f1f08a1f1
Nhlanhla Vilakazi <villakazi.nhlanhla@gmail.com>
2023-09-19 06:20:14 +0200

    DataContext setup

---
+++ ManagePeople.Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagePeople.Data
+{
+    public class DataContext : DbContext
+    {
+        public new DbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(new SqlConnection(ServicesExtensions.defaultConnectionString));
+        }
+    }
+}
---
+++ ManagePeople.Data/ServicesExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ManagePeople.Data
+{
+    public class ServicesExtensions
+    {
+        public static string? defaultConnectionString { get; set; }
+        public static void CheckApplicationConnectionString()
+        {
+            defaultConnectionString = Environment.GetEnvironmentVariable("PeopleManagementConnectionString");
+            if (string.IsNullOrEmpty(defaultConnectionString))
+                throw new Exception("Please setup the connection string on your environment variables");
+        }
+    }
+}
