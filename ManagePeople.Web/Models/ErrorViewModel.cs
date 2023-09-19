b04f6218a907874f21e7a3c1a10c9020293539d3
Nhlanhla Vilakazi <villakazi.nhlanhla@gmail.com>
2023-09-19 05:47:21 +0200

    Add project files.

---
+++ ManagePeople.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagePeople.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ManagePeople.Web/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+    $(".register-link").click(function () {
+        $(".wrapper").addClass("active");
+    });
+
+    $(".login-link").click(function () {
+        $(".wrapper").removeClass("active");
+    });
+
+    $(".login-btn").click(function () {
+        if ($(".wrapper").hasClass("popup-active")) {
+            $(".wrapper").removeClass("popup-active");
+        }
+        else {
+            $(".wrapper").addClass("popup-active");
+        }
+    });
+
+    $(".icon-close").click(function () {
+        $(".wrapper").removeClass("popup-active");
+    });
+});
